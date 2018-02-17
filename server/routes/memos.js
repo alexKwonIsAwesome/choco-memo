d5eb129bb93bd1e82d2399151a09354f420fa727
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-02-17 15:10:49 +0900

    Refactored router with async/await function

--- server/routes/memos.js
+++ server/routes/memos.js
@@ -6,96 +6,93 @@ var _ = require('lodash');
 var { Memo } = require('./../models/memo');
 
 // GET memos
-router.get('/', (req, res) => {
-    Memo.find()
-        .then((memo) => {
-            res.send({ memo });
-        })
-        .catch((e) => {
-            res.status(400).send(e);
-        })
+router.get('/', async (req, res) => {
+    try {
+        const memos = await Memo.find();
+        res.send({ memos });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 // POST memo item
-router.post('/', (req, res) => {
-    var memo = new Memo({
-        title: req.body.title,
-        contents: req.body.text,
-        createdAt: new Date
-    });
-
-    memo.save()
-        .then((memo) => {
-            res.send(memo);
-        })
-        .catch((e) => {
-            res.status(400).send(e);
+router.post('/', async (req, res) => {
+    try {
+        const memoInstance = new Memo({
+            title: req.body.title,
+            contents: req.body.contents,
+            createdAt: new Date
         });
+        const memo = await memoInstance.save();
+        res.send(memo);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 // GET memo item
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+        if (!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
+
+        const memo = await Memo.findById(id);
+        
+        if (!memo) {
+            res.status(404).send();
+        }
+        res.send(memo);
+    } catch (e) {
+        res.status(400).send(e);
     }
-
-    Memo.findById(id)
-        .then((memo) => {
-            if (!memo) {
-                res.status(404).send();
-            }
-            res.send(memo);
-        })
-        .catch((e) => {
-            res.status(400).send(e);
-        })
 });
 
 // UPDATE memo item
-router.patch('/:id', (req, res) => {
-    const id = req.params.id;
-    const body = _.pick(req.body, ['title', 'contents', 'isArchived']);
+router.patch('/:id', async (req,res) => {
+    try {
+        const id = req.params.id;
+        const body = _.pick(req.body, ['title', 'contents', 'isArchived']);
+    
+        if (!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+        body.updatedAt = new Date;
+    
+        const memo = await Memo.findByIdAndUpdate(id, { $set: body });
+    
+        if (!memo) {
+            res.status(404).send();
+        }
+        res.send(memo);
+    } catch (e) {
+        res.status(400).send(e);
     }
-
-    body.updatedAt = new Date;
-    Memo.findByIdAndUpdate(id, { $set: body })
-        .then((memo) => {
-            if (!memo) {
-                res.status(404).send();
-            }
-
-            res.send(memo);
-        })
-        .catch((e) => {
-            res.status(400).send();
-        })
-    res.send(body);
 });
 
 // DELETE memo item
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+        if (!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
+    
+        const memo = await Memo.findByIdAndRemove(id);
+    
+        if (!memo) {
+            res.status(404).send();
+        }
+
+        res.send(memo);
+    } catch (e) {
+        res.status(400).send(e);
     }
 
-    Memo.findByIdAndRemove(id)
-        .then((memo) => {
-            if (!memo) {
-                res.status(404).send();
-            }
-
-            res.send(memo);
-        })
-        .catch((e) => {
-            res.status(400).send();
-        })
 });
 
 module.exports = router;
